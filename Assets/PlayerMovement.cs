5bd9136e64d862d24a1a8e689baf99f847b1f2bd
Manuel <25lopezm25@gmail.com>
2021-07-01 22:34:30 -0600

    se añadio el sistema de movimiento del jugador

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] float speedMovement;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravity = -9.81f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance;
+    [SerializeField] LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
         
+
+        Vector3 movement = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+
+        //aplys the movement
+        controller.Move(movement * speedMovement * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //aplys gravity
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
