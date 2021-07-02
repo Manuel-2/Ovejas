ad597460d1ae44539a10eddd5e9d09182fc28380
Manuel <25lopezm25@gmail.com>
2021-07-01 21:57:08 -0600

    se añadio script para controlar la camara del jugador

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float sensitivity;
+    [SerializeField] Transform playerBody;
+
+    Vector2 mouseMovement;
+    float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseMovement.x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseMovement.y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseMovement.y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseMovement.x);
+    }
+}
