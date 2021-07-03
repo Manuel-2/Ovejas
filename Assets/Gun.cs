3f0e5f8fd4a68a49db175283384d799ce41dfcaa
Manuel <25lopezm25@gmail.com>
2021-07-02 19:20:10 -0600

    se añadieron las bases para el sistema de disparos del jugador

---
+++ Assets/Gun.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] Camera fpsCam;
+    [SerializeField] float damage;
+    [SerializeField] float range;
+    [SerializeField] float fireRate;
+    float nextTimeToFire;
+
+    [SerializeField] GameObject impactEfect;
+    [SerializeField] float impactForce;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f/fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage();
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce( (hit.point - this.transform.position).normalized * impactForce,ForceMode.Impulse);
+            }
+
+            GameObject hitEfect = Instantiate(impactEfect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(hitEfect, 1f);
+            
+        }
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] float healt;
+
+    public void TakeDamage()
+    {
+        transform.localScale = transform.localScale * 0.95f;
+        //Die();
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+    
+}
