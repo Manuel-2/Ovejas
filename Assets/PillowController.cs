186b5434a5e17172fbbbc4c1549ba9b2a8b340c8
Manuel <25lopezm25@gmail.com>
2021-07-04 19:34:35 -0600

    ahora el jugador puede recojer almohadas y lanzarlas

---
+++ Assets/PillowController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillowController : MonoBehaviour
+{
+    [SerializeField] float canRecolectDelay;
+
+
+    private void Awake()
+    {
+        GetComponent<SphereCollider>().enabled = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("CanBeRecolected",canRecolectDelay);
+    }
+
+    void CanBeRecolected()
+    {
+        GetComponent<SphereCollider>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField] GameObject Pillow;
+    bool hasPillow = false;
+
+    [SerializeField] GameObject PillowPrefab;
+    [SerializeField] float launchForce;
+    [SerializeField] Transform launchPoint;
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && hasPillow)
+        {
+            TrowPillow();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pillow"))
+        {
+            //checks if currently the player as a pillow equiped if he hasn't takes the pillow for the groud
+            if(hasPillow == false)
+            {
+                TakePillow(other.gameObject);
+            }
+        }
+    }
+
+    private void TakePillow(GameObject pillow)
+    {
+        hasPillow = true;
+        Pillow.SetActive(hasPillow);
+        
+        Destroy(pillow);
+    }
+
+    private void TrowPillow()
+    {
+        hasPillow = false;
+        Pillow.SetActive(hasPillow);
+
+        //intanciar una nueva almohada
+        GameObject lauchedPillow = Instantiate(PillowPrefab, launchPoint.position,launchPoint.rotation);
+        //ejercer un impulso a esa almohada
+        lauchedPillow.GetComponent<Rigidbody>().AddForce(launchPoint.forward * launchForce,ForceMode.Impulse);
+
+    }
+
+}
