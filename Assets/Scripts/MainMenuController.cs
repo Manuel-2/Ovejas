4582626f539479948687878d375e1a0ee8bb3295
Manuel <25lopezm25@gmail.com>
2021-08-10 14:38:23 -0600

    cuando el jugador da click en la pantalla de incio se carga testRoom00

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] string GameSceneName;
+    public void StartGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(GameSceneName);
     }
 }
