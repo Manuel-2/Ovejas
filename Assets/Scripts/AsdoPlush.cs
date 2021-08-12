9cf7c072d99134a8facf9ba0f5f443cb255dc140
Manuel <25lopezm25@gmail.com>
2021-08-11 18:35:33 -0600

    el peluche de asdo hace un sonido de hule o de patito

---
+++ Assets/Scripts/AsdoPlush.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsdoPlush : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] string playerTag, pillowTag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            audioSource.Play();
+        }
+    }
+}
