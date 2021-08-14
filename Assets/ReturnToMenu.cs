b381845f72fac7f80fb7307c5dd1164e5e51fe8d
Manuel <25lopezm25@gmail.com>
2021-08-14 15:40:20 -0600

    se completo la escena del final

---
+++ Assets/ReturnToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    [SerializeField] string mainMenuName;
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuName);
+    }
+}
